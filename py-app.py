6ddf390f8f829ed422e33ad0b2d85f86ccf956c5
lpastor74 <plaslo@gmail.com>
2019-09-24 23:49:41 -0500

    add

--- py-app.py
+++ py-app.py
@@ -1,12 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash, make_response, session, g
+import requests
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_whale():
-    return 'Whale, Hello there!'
+    #auth = request.cookies.get('accs_tkn')
+    res = requests.get('https://favqs.com/api/qotd')
+    y = json.loads(res.text)
+    author = y['quote']['author']
+    body = y['quote']['body']
+
+    return render_template('index.html', athr=author, bdy=body)
+
+
+@app.route('/apicall')
+def api_call():
+    #auth = request.cookies.get('accs_tkn')
+    res = requests.get('http://py-api:5000/api/v1/resources/user/1')
+    y = json.loads(res.text)
+    data = y['data']
+    user = y['user']
+
+    return render_template('api.html', athr=user, bdy=data)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, port=int("5024"), host='0.0.0.0')
